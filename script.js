d1909f5a0a09a71ccd152f6d2ed499a2d82427c7
Diego Cardenas <109765105+diegoCardenas03@users.noreply.github.com>
2025-03-05 13:00:33 -0300

    Que al buscar peliculas aparezcan en español

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&language=es-MX&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const searchURL = BASE_URL + '/search/movie?&language=es-MX&'+API_KEY;
 
 const genres = [
     {
